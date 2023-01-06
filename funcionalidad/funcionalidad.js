815f2e33d37cb5b12e110b7b7f9b2af00e953401
Guillermo De Dios <guille_dedios03@hotmail.com>
2023-01-06 17:24:44 -0300

    Nuevo Terminado

---
+++ funcionalidad/funcionalidad.js
@@ -0,0 +1,82 @@
+//calcula los años para los datos personales
+var fechaactual = new Date();
+var fechanacimiento = new Date(1963, 1, 5)
+document.getElementById("cumpleaños").innerHTML += fechaactual.getFullYear()-fechanacimiento.getFullYear();
+
+function sacar(){
+    for (let index = 1; index <= 5; index++) {
+        if(document.getElementById("vermas"+index).style.display == "block"){
+            document.getElementById("vermas"+index).style = 'display:none';
+            document.getElementById("btnvermenos"+index).style = 'display:none';
+            document.getElementById("btnvermas"+index).style = 'display:block';
+        }
+    }
+}
+
+//botones ver mass
+
+//quise generalizar la funcion de los botones ver mas y ver menos pero la parte addEventListener("click",function())
+//no me deja mandar por parametro el numero del boton correspondiente
+//ej. addEventListener("click",funcioncreada("1"))
+document.getElementById("btnvermas1").addEventListener("click",function(){
+    sacar();
+    document.getElementById("vermas1").style = 'display:block';
+    document.getElementById("btnvermas1").style = 'display:none';
+    document.getElementById("btnvermenos1").style = 'display:block';
+});
+
+document.getElementById("btnvermas2").addEventListener("click",function(){
+    sacar();
+    document.getElementById("vermas2").style = 'display:block';
+    document.getElementById("btnvermas2").style = 'display:none';
+    document.getElementById("btnvermenos2").style = 'display:block';
+})
+
+document.getElementById("btnvermas3").addEventListener("click",function(){
+    sacar();
+    document.getElementById("vermas3").style = 'display:block';
+    document.getElementById("btnvermas3").style = 'display:none';
+    document.getElementById("btnvermenos3").style = 'display:block';
+})
+
+document.getElementById("btnvermas4").addEventListener("click",function(){
+    sacar();
+    document.getElementById("vermas4").style = 'display:block';
+    document.getElementById("btnvermas4").style = 'display:none';
+    document.getElementById("btnvermenos4").style = 'display:block';
+})
+
+document.getElementById("btnvermas5").addEventListener("click",function(){
+    sacar();
+    document.getElementById("vermas5").style = 'display:block';
+    document.getElementById("btnvermas5").style = 'display:none';
+    document.getElementById("btnvermenos5").style = 'display:block';
+})
+
+
+//botones ver menos
+document.getElementById("btnvermenos1").addEventListener("click",function(){
+    document.getElementById("vermas1").style = 'display:none';
+    document.getElementById("btnvermenos1").style = 'display:none';
+    document.getElementById("btnvermas1").style = 'display:block';
+})
+document.getElementById("btnvermenos2").addEventListener("click",function(){
+    document.getElementById("vermas2").style = 'display:none';
+    document.getElementById("btnvermenos2").style = 'display:none';
+    document.getElementById("btnvermas2").style = 'display:block';
+})
+document.getElementById("btnvermenos3").addEventListener("click",function(){
+    document.getElementById("vermas3").style = 'display:none';
+    document.getElementById("btnvermenos3").style = 'display:none';
+    document.getElementById("btnvermas3").style = 'display:block';
+})
+document.getElementById("btnvermenos4").addEventListener("click",function(){
+    document.getElementById("vermas4").style = 'display:none';
+    document.getElementById("btnvermenos4").style = 'display:none';
+    document.getElementById("btnvermas4").style = 'display:block';
+})
+document.getElementById("btnvermenos5").addEventListener("click",function(){
+    document.getElementById("vermas5").style = 'display:none';
+    document.getElementById("btnvermenos5").style = 'display:none';
+    document.getElementById("btnvermas5").style = 'display:block';
+})
